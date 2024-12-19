79444076cea02940458585fffa44d2fbbd40c157
vikrantsharmavs <infovikrantsharma@gmail.com>
2024-12-20 00:09:27 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const inputs = {
+  username: {
+    element: document.getElementById("username"),
+  },
+  email: {
+    element: document.getElementById("email"),
+  },
+  role: {
+    element: document.getElementById("role"),
+  },
+  password: {
+    element: document.getElementById("password"),
+  },
+  confirmPassword: {
+    element: document.getElementById("confirmPassword"),
+  },
+};
